11e1fcab68620b73c6c1b6537fa54625bad003de
Marisa McLean <marisa.mclean@uon.edu.au>
2019-04-04 04:00:10 +0000

    Print menu selection statement

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-main()
+int main()
 {
-  
-  return 0;
+   // char choice; // To assign the user's choice of options to
+   // do
+  //  {
+        printf("Do you want to;\na) Encrypt with a rotation cipher\nb) Decrypt a rotation cipher\nc) Encrypt with a substitution cipher\nd) Decrypt a substitution ciper\nYour choice: "); // Offers a choice of what the code should do and prompts the user to select one
+        //scanf("%c\n", choice);
+ //   }
+   // while(choice != a, choice != b, choice != c, choice != d); // Do while loop prevents error from incorrect intput i.e. none of the options
+
+    return 0;
 }
