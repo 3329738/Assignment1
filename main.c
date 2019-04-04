7e5e71f88c1f4bc8de689e5f6e50f920c13c82b5
Marisa McLean <marisa.mclean@uon.edu.au>
2019-04-04 04:14:11 +0000

    lotsa warnings

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main()
 {
-   // char choice; // To assign the user's choice of options to
-   // do
-  //  {
+   char choice; // To assign the user's choice of the options to
+   char a, b, c, d;
+   do
+   {
         printf("Do you want to;\na) Encrypt with a rotation cipher\nb) Decrypt a rotation cipher\nc) Encrypt with a substitution cipher\nd) Decrypt a substitution ciper\nYour choice: "); // Offers a choice of what the code should do and prompts the user to select one
-        //scanf("%c\n", choice);
- //   }
-   // while(choice != a, choice != b, choice != c, choice != d); // Do while loop prevents error from incorrect intput i.e. none of the options
+        scanf("%c\n", choice); // To read the users input to make a choice
+   }
+   while((choice != a) || (choice != b) || (choice != c) || (choice != d)); // Do while loop prevents error from incorrect intput i.e. none of the options
 
     return 0;
 }
