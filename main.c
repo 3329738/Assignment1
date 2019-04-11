23f085fbe4d8fcf8c4f7c8293697fdabdf21c1d4
Marisa McLean <marisa.mclean@uon.edu.au>
2019-04-11 05:50:10 +0000

    Might work?

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-char cipher(string, key)
+char cipher(int string, int key);
 
 int main(){
    int key;
-   int string;
-   if ( key>26 ){
+   char alphabet[] = {a, b, c, d, e, f, g, h, i, j, k, l ,m , n, o, p, q, r, s, t, u, v, w, x, y, z};
+   
+   
+   if (key > 26){
         key = 0
     }
     char ciper(string, key); 
@@ -13,5 +15,8 @@ int main(){
     
 }
 
-char cipher(int string, int key);
-string 
+char encrypted(int string, int key){
+    char decrypted;
+    decrypted = (string + key)(%26);
+    return decrypted;
+}
