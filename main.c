a4dc7982976079aaa2f51797901546ec49381e9e
Marisa McLean <marisa.mclean@uon.edu.au>
2019-04-11 05:04:35 +0000

    Maybe right track??

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-
+char cipher(string, key)
 
 int main(){
-    char coder[100] ={a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
-    int 
+   int key;
+   int string;
+   if ( key>26 ){
+        key = 0
+    }
+    char ciper(string, key); 
+   
+    
 }
+
+char cipher(int string, int key);
+string 
